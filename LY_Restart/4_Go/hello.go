16328d1c261b5962ed3bbd59aa1be87b295c729f
Hcy2ly <1757256226@qq.com>
2020-02-04 23:51:48 +0800

    update 3_mongodb

---
+++ LY_Restart/4_Go/hello.go
@@ -0,0 +1,15 @@
+/*
+ * @Author: ly
+ * @Date: 2020-02-04 23:26:33
+ * @LastEditTime : 2020-02-04 23:38:39
+ * @LastEditors  : Please set LastEditors
+ * @Description: In User Settings Edit
+ * @FilePath: \beixiang\LY_Restart\4_Go\hello.go
+ */
+package main;
+
+import "fmt";
+
+func main() {
+    fmt.Println("Hello, World!");
+}
